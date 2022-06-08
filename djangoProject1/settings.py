56c018ae1e113e6b9d0b011795b3afb4edf8e532
Yurii Ushakov <lures232@gmail.com>
2022-06-08 15:15:55 +0300

    CI/CD

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -93,6 +93,32 @@ DATABASES = {
     }
 }
 
+
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_DATABASE = os.environ.get("DB_DATABASE")
+DB_IS_AVAIL = all([
+        DB_USERNAME,
+        DB_PASSWORD,
+        DB_HOST,
+        DB_PORT,
+        DB_DATABASE
+])
+
+if DB_IS_AVAIL:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": DB_DATABASE,
+            "USER": DB_USERNAME,
+            "PASSWORD": DB_PASSWORD,
+            "HOST": DB_HOST,
+            "PORT": DB_PORT,
+        }
+    }
+
 AWS_ACCESS_KEY_ID = 'AKIA2GY6R3KGVW75LPP3'
 AWS_SECRET_ACCESS_KEY = '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'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
