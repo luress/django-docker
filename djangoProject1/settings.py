d75419969c5fc764207ddf05282cfd5feb031adf
Yurii Ushakov <lures232@gmail.com>
2022-06-08 17:07:02 +0300

    CI/CD

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -160,11 +160,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/styles.css/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join('static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
