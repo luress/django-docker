9b304a34e16a7325dd85598ee06fa84beeab7d04
Yurii Ushakov <lures232@gmail.com>
2022-06-17 16:27:46 +0300

    change some features in song containers

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 AUTH_USER_MODEL = 'project.CustomUser'
--- static/script.js
+++ static/script.js
@@ -13,17 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const audioTitle = document.getElementById('audio-title')
 
 
+    let song_area_id = undefined
     let playlist = undefined
     let playState = 'play';
     let are_playing = false
     let rAF = null;
     let song_id = undefined
     let tmp = 0
+    let current_song = undefined
 
     const play = () => {
         playState = 'pause'
         are_playing = true
         audio.play();
+        playIconContainer.id = "pause-button"
         requestAnimationFrame(whilePlaying);
     }
 
@@ -32,6 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
         cancelAnimationFrame(rAF);
         playState = 'play'
         are_playing = false
+        playIconContainer.id = "play-button"
     }
 
     const load_music = () => {
@@ -46,6 +50,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     const allContainer = document.createElement('div')
                     const imageContainer = document.createElement('div')
                     const artist = document.createElement('p')
+                    const imageMiddle = document.createElement('div')
+                    const songAreaImagePlay = document.createElement('div')
                     
                     const temp = document.querySelector('.song-list')
                     temp.append(song_area)
@@ -56,11 +62,18 @@ document.addEventListener('DOMContentLoaded', function() {
                     artist.innerHTML = songs[i].artist
                     title.innerHTML = songs[i].title
 
+                    songAreaImagePlay.classList = 'song-area-image-play'
+
+                    imageMiddle.className = 'image-middle'
+                    imageMiddle.append(songAreaImagePlay)
+
 
                     song_area.className = 'song-area'
 
                     imageContainer.className = 'song-area-image-container'
                     imageContainer.append(image)
+                    imageContainer.append(imageMiddle)
+                    
                     
                     allContainer.className = 'song-all-container'
                     allContainer.append(imageContainer)
@@ -69,21 +82,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
                     song_area.append(allContainer)
 
+                    if(song_id !== songs[i].id) {
+                        songAreaImagePlay.classList = 'song-area-image-play'
+                    }
 
 
                     song_area.addEventListener('click', () => {
+                        
                         if(are_playing === false) {
                             if(song_id !== songs[i].id){
+                                songAreaImagePlay.classList = 'song-area-image-pause'
+                                
                                 imageSong.setAttribute('src', songs[i].image)
                                 song.setAttribute('src', songs[i].audio_file)
                                 audioTitle.innerHTML = songs[i].title
                                 audioArtist.innerHTML = songs[i].artist
                                 song_id = songs[i].id
+                                current_song = songAreaImagePlay
                                 play()
+                                song_area_id = songs[i].id
                                 audioInfo.classList.remove('hidden')
                                 audioInfo.style.bottom = '0'
                                 document.getElementById('music-slider').style.bottom = '90px'
-
                                 fetch(`/playlist/${song_id}`)
                                     .then(response=>response.json())
                                     .then(data => {
@@ -92,16 +112,21 @@ document.addEventListener('DOMContentLoaded', function() {
                                     })
 
                                 } else {
+                                    songAreaImagePlay.classList = 'song-area-image-pause'
                                     play()
                                 }
                         } else {
                             if(song_id === songs[i].id){
+                                songAreaImagePlay.classList = 'song-area-image-play'
                                 pause()
                             } else {
+                                songAreaImagePlay.classList = 'song-area-image-pause' 
                                 imageSong.setAttribute('src', songs[i].image)
                                 song.setAttribute('src', songs[i].audio_file)
                                 audioTitle.innerHTML = songs[i].title
                                 audioArtist.innerHTML = songs[i].artist
+                                current_song.classList ='song-area-image-play'
+                                current_song = songAreaImagePlay
                                 fetch(`/playlist/${song_id}`)
                                     .then(response=>response.json())
                                     .then(data => {
@@ -109,7 +134,7 @@ document.addEventListener('DOMContentLoaded', function() {
                                         console.log(playlist)
                                     })
                                 song_id = songs[i].id
-
+                                   
                                 play()
                             }
 
@@ -203,8 +228,14 @@ document.addEventListener('DOMContentLoaded', function() {
         displayBufferedAmount();
     });
 }
-
-
+    const play_pause = () => {
+        if(are_playing === true){
+            
+        }
+        
+    
+}
+    
 
     musicSlider.addEventListener('input', (e) => {
         showRangeProgress(e.target);
@@ -237,4 +268,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         });
     load_music()
+    play_pause()
 })
